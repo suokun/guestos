bb5b1bd7a18b476ddeba956a6b88c363d54a07aa
suokun <suokun24@126.com>
2015-03-29 19:23:13 -0600

    modified:   include/xen/interface/sched.h

--- include/xen/interface/sched.h
+++ include/xen/interface/sched.h
@@ -97,6 +97,19 @@ struct sched_watchdog {
     uint32_t timeout;           /* timeout */
 };
 
+
+//add by Luwei
+#define SCHEDOP_sleep 11
+struct sched_sleep {
+	    uint64_t timeout;
+};
+DEFINE_GUEST_HANDLE_STRUCT(sched_sleep);
+//end
+
+
+
+
+
 /*
  * Reason codes for SCHEDOP_shutdown. These may be interpreted by control
  * software to determine the appropriate action. For the most part, Xen does
